5348cad6e11884d2b7314535e8cb189a8e431994
root <root@1192745-co48305.tw1.ru>
2023-02-05 19:51:44 +0300

    Update

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
 import os
-import requests
-import youtube_dl
+import subprocess
 
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }]
-}
-
-# specify the URL of the playlist
+# YouTube playlist URL
 playlist_url = "https://www.youtube.com/playlist?list=PLOAFzRws139CoGX2UvsGx5yWplrxBB6Hh"
 
-# download the audio files from the playlist
-try:
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([playlist_url])
-except Exception as e:
-    print(e)
-    sys.exit(1)
+# Output directory for the downloaded files
+output_dir = "/"
 
-def send_file(file_path, chat_id, token):
-    file = {'document': (os.path.basename(file_path), open(file_path, 'rb'))}
-    data = {'chat_id': chat_id}
-    response = requests.post(f'https://api.telegram.org/bot{token}/sendDocument', data=data, files=file)
-    return response.json()
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
-current_directory = os.getcwd()
-files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
+# The number of videos to download per request
+download_count = 10
 
-chat_id = "-895143844"
-token = "5640044198:AAFmq7K5uA7TtJaX2ku41cWdRjPXpRkAY6k"
+# Counter for the total number of videos downloaded
+total_downloaded = 0
 
-for file in files:
-    file_path = os.path.join(current_directory, file)
-    send_file(file_path, chat_id, token)
+# Loop through all the videos in the playlist
+while True:
+    # YouTube-dl command to download the videos as audio files
+    command = f"youtube-dl -o '{output_dir}/%(title)s.%(ext)s' --extract-audio --audio-format mp3 --playlist-end {download_count} {playlist_url}"
+
+    # Execute the command and capture the output
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # If the command completed successfully, break out of the loop
+    if result.returncode == 0:
+        break
+
+    # If the command failed, print the error messages and skip the failed videos
+    error_messages = result.stderr.decode("utf-8").strip().split("\n")
+    failed_videos = []
+    for error_message in error_messages:
+        if "ERROR:" in error_message:
+            video_title = error_message.split(" ")[-1]
+            failed_videos.append(video_title)
+    print(f"The following videos could not be downloaded: {failed_videos}")
+
+    # Update the playlist URL to skip the failed videos
+    playlist_url = f"{playlist_url}&index={total_downloaded + download_count}"
+    total_downloaded += download_count
