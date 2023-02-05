27bc9d665391cc40a02052a907bc92a19177d119
root <root@1192745-co48305.tw1.ru>
2023-02-05 22:41:40 +0300

    NIGHT UPDATE 3

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
 import os
-import subprocess
+import youtube_dl
+import requests
 
-# YouTube playlist URL
-playlist_url = "https://www.youtube.com/playlist?list=PLOAFzRws139CoGX2UvsGx5yWplrxBB6Hh"
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'ignoreerrors': True,
+    'continue_dl': True
+}
 
-# Output directory for the downloaded files
-output_dir = "/"
+BOT_TOKEN = "5640044198:AAFmq7K5uA7TtJaX2ku41cWdRjPXpRkAY6k"
+GROUP_CHAT_ID = "8951433844"
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+def send_audio_to_telegram(audio_file_path):
+    audio = open(audio_file_path, 'rb')
+    files = {'audio': ('audio.mp3', audio)}
+    response = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendAudio',
+                             data={'chat_id': GROUP_CHAT_ID}, files=files)
+    if response.status_code != 200:
+        print("Failed to send audio to Telegram group: " + response.text)
 
-# The number of videos to download per request
-download_count = 10
-
-# Counter for the total number of videos downloaded
-total_downloaded = 0
-
-# Loop through all the videos in the playlist
-while True:
-    # YouTube-dl command to download the videos as audio files
-    command = f"youtube-dl -o '{output_dir}/%(title)s.%(ext)s' --extract-audio --audio-format mp3 --playlist-end {download_count} {playlist_url}"
-
-    # Execute the command and capture the output
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # If the command completed successfully, break out of the loop
-    if result.returncode == 0:
-        break
-
-    # If the command failed, print the error messages and skip the failed videos
-    error_messages = result.stderr.decode("utf-8").strip().split("\n")
-    failed_videos = []
-    for error_message in error_messages:
-        if "ERROR:" in error_message:
-            video_title = error_message.split(" ")[-1]
-            failed_videos.append(video_title)
-    print(f"The following videos could not be downloaded: {failed_videos}")
-
-    # Update the playlist URL to skip the failed videos
-    playlist_url = f"{playlist_url}&index={total_downloaded + download_count}"
-    total_downloaded += download_count
+playlist_url = input("Enter the URL of the YouTube playlist: ")
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(playlist_url, download=False)
+    videos = info['entries']
+    for video in videos:
+        title = video['title'] + '.mp3'
+        if os.path.isfile(title):
+            print("File already exists: " + title)
+            continue
+        ydl.download([video['webpage_url']])
