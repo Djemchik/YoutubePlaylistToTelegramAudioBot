269bc3d39f7be74df9b5e748e1f81c619e94cbcd
Djem Tem <Djemchik@proton.me>
2023-02-06 19:23:28 +0300

    Update 06_02_2023

--- main.py
+++ main.py
@@ -2,28 +2,31 @@ import os
 import youtube_dl
 import requests
 
+download_folder = "downloaded_audios"
+if not os.path.exists(download_folder):
+    os.makedirs(download_folder)
 ydl_opts = {
     'format': 'bestaudio/best',
-    'outtmpl': '%(title)s.%(ext)s',
+    'outtmpl': download_folder + '/%(title)s.%(ext)s',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'ignoreerrors': True,
-    'continue_dl': True
 }
 
 BOT_TOKEN = "5640044198:AAFmq7K5uA7TtJaX2ku41cWdRjPXpRkAY6k"
-GROUP_CHAT_ID = "8951433844"
+GROUP_CHAT_ID = "-1001874282744"
 
 def send_audio_to_telegram(audio_file_path):
-    audio = open(audio_file_path, 'rb')
-    files = {'audio': ('audio.mp3', audio)}
-    response = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendAudio',
-                             data={'chat_id': GROUP_CHAT_ID}, files=files)
-    if response.status_code != 200:
-        print("Failed to send audio to Telegram group: " + response.text)
+	audio_filename = os.path.basename(audio_file_path)
+	audio = open(audio_file_path, 'rb')
+	files = {'audio': (audio_filename, audio)}
+	response = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendAudio',
+	data={'chat_id': GROUP_CHAT_ID}, files=files)
+	if response.status_code != 200:
+		print("Failed to send audio to Telegram group: " + response.text)
+
 
 playlist_url = input("Enter the URL of the YouTube playlist: ")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -31,7 +34,10 @@ with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     videos = info['entries']
     for video in videos:
         title = video['title'] + '.mp3'
-        if os.path.isfile(title):
-            print("File already exists: " + title)
+        file_path = os.path.join(download_folder, title)
+        if os.path.isfile(file_path):
+            print("File already exists, skipping download: " + title)
+            send_audio_to_telegram(file_path)
             continue
         ydl.download([video['webpage_url']])
+        send_audio_to_telegram(file_path)
