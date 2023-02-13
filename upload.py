8fbd833e1ecfdb75c7308a4a790d885191b28455
Djemchik <jemil1.2004@mail.ru>
2023-02-14 00:54:50 +0300

    14 02 23 UPDATE 3

--- upload.py
+++ upload.py
@@ -20,14 +20,30 @@ def send_to_telegram(file_path):
 def is_audio_file(file):
     return file.endswith('.mp3') or file.endswith('.wav') or file.endswith('.ogg')
 
-folder = "downloaded_audios"
+def display_directories():
+    directories = [d for d in os.listdir() if os.path.isdir(d)]
+    for i, directory in enumerate(directories):
+        print(f"{i+1}. {directory}")
+
+display_directories()
+selected_directory = int(input("Please enter the number of the directory you would like to upload files from: "))
+directories = [d for d in os.listdir() if os.path.isdir(d)]
+folder = directories[selected_directory - 1]
+
 files = os.listdir(folder)
 total_files = len(files)
 processed_files = 0
+start = time.time()
 for file in files:
     if is_audio_file(file):
         file_path = os.path.join(folder, file)
         send_to_telegram(file_path)
-        time.sleep(5)
         processed_files += 1
-        print(f"Upload progress: {100 * processed_files / total_files:.2f}%")
+        time_elapsed = time.time() - start
+        estimated_time_left = (time_elapsed / processed_files) * (total_files - processed_files)
+        print(f"{processed_files} of {total_files} files processed. {file} sent. Estimated time left: {estimated_time_left:.2f} seconds")
+        time.sleep(5)
+        
+        
+
+
